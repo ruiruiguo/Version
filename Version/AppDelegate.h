6f3cf67da30d7a84c1426594bd76114955c573a2
ruiruiguo <magus@magus-tao.local>
2015-09-18 10:58:53 +0800

    Initial Commit

---
+++ Version/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Version
+//
+//  Created by magus on 15/9/18.
+//  Copyright (c) 2015年 magus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
